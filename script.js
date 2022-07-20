d96d9451b89ac0caca7c4a1b19f79e0ba3b1a39c
valentina bentivegna <valenbenti428@gmail.com>
2022-07-20 18:39:06 -0300

    primer

---
+++ script.js
@@ -0,0 +1,23 @@
+let hi = prompt("Hola! Sos mayor de 18 anos? 'la respuesta es 'si'") ;
+while(hi!= "si") {
+    alert("ingresaste " + hi);
+    hi = prompt("Respondeme con un 'si' no seas malooo")
+}
+
+let tomasMucho = prompt("Gracias por la primera respuesta, ahora te pregunto sos de beber alchol seguido?'si'");
+while(tomasMucho != "si") {
+    alert("ingresaste " + tomasMucho);
+    tomasMucho = prompt("Respondeme con un 'si' por favor ")
+}
+
+let estoyProbando = prompt("Gracias por colaborar conmigo espero que no hayas perdido mucho tiempo respondeme con un 'tranqui'");
+while(estoyProbando != "tranqui") {
+    alert("ingresaste " + estoyProbando);
+    estoyProbando = prompt("solo respondeme con un 'tranqui'");
+}
+
+let referenciaSimpson = prompt("Francisco: Debo dejarte. gracias por ver mi entrega espero que este bien jajaj acepto la critica soy nuevisima en esto y me esta costando bastante  solo respondeme con un  'vospodes' sisi las dos palabras juntas asi eso me da animo! jajajaja.");
+while(referenciaSimpson != "vospodes"){
+alert("ingresaste " + referenciaSimpson);
+referenciaSimpson = prompt("despideme con un 'vospodes'");
+}
